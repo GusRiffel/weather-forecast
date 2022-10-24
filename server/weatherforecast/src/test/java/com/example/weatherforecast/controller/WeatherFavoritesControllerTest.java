43c4acb588b4b8684e09bbf6d5c4086460bbe8cb
Gustavo Riffel <5riffg16@solent.ac.uk>
2022-10-24 21:13:05 +0100

    Starts tests for weather favourite

---
+++ server/weatherforecast/src/test/java/com/example/weatherforecast/controller/WeatherFavoritesControllerTest.java
@@ -0,0 +1,25 @@
+package com.example.weatherforecast.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WeatherFavoritesControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void delete() {
+    }
+}
---
+++ server/weatherforecast/src/test/java/com/example/weatherforecast/service/WeatherFavoritesServiceTest.java
@@ -0,0 +1,25 @@
+package com.example.weatherforecast.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WeatherFavoritesServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void listAllCitiesByUsername() {
+    }
+
+    @Test
+    void deleteCityByUsername() {
+    }
+
+    @Test
+    void save() {
+    }
+}
